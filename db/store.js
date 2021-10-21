c1601bd734cd1578fa83f2ea322ce7dc63f31fde
Mohamed Lazrek <mohamed.lazrekb@gmail.com>
2021-10-21 16:11:08 -0400

    files structure

---
+++ db/store.js
@@ -0,0 +1,54 @@
+// dependencies
+const util = require('util');
+const fs = require('fs');
+const { v4: uuidv4 } = require('uuid'); 
+
+
+const readNote = util.promisify(fs.readFile);
+const writeNote = util.promisify(fs.writeFile);
+
+class Save {
+    write(note) {
+        return writeNote('db/db.json', JSON.stringify(note));
+    }
+
+    read() {
+        return readNote('db/db.json', 'utf8');
+    }
+
+    retrieveNotes() {
+        return this.read().then(notes => {
+            let parsedNotes;
+            try {
+                parsedNotes = [].concat(JSON.parse(notes));
+            } catch (err) {
+                parsedNotes = [];
+            }
+            return parsedNotes;
+        });
+    }
+
+    addNote(note) {
+        const { title, text } = note;
+        if (!title || !text) {
+            throw new Error('Both title and text can not be blank');
+        }
+        // uuid package to add ids
+        const newNote = { title, text, id: uuidv4() };
+
+        // Retrieve Notes, add the new note, update notes
+        return this.retrieveNotes()
+            .then(notes => [...notes, newNote])
+            .then(updatedNotes => this.write(updatedNotes))
+            .then(() => newNote);
+    }
+
+    // delete notes
+    deleteNote(id) {
+        return this.retrieveNotes()
+            .then(notes => notes.filter(note => note.id !== id))
+            .then(filteredNotes => this.write(filteredNotes));
+    }
+}
+
+module.exports = new Save();
---
+++ server.js
@@ -0,0 +1,28 @@
+// dependencies
+const express = require('express');
+
+// route files
+const apiRoutes = require('./routes/apiRoutes');
+const htmlRoutes = require('./routes/htmlRoutes');
+
+
+const app = express();
+
+// set PORT
+const PORT = process.env.PORT || 3001;
+
+// parse string or array data
+app.use(express.urlencoded({ extended: true }));
+
+// parse JSON data
+app.use(express.json());
+
+
+app.use(express.static('public'));
+app.use('/api', apiRoutes);
+app.use('/', htmlRoutes);
+
+// event listener
+app.listen(PORT, () => {
+    console.log(`Open server on port http://localhost:${PORT} `);
+});
