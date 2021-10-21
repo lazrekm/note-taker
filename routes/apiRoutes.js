6f7861c086ce9311ea5ece9735586f15e4a2a3f2
Mohamed Lazrek <mohamed.lazrekb@gmail.com>
2021-10-21 16:24:58 -0400

    Api routes

---
+++ routes/apiRoutes.js
@@ -0,0 +1,31 @@
+// dependencies
+const router = require('express').Router();
+
+const saveData = require('../db/store');
+
+// get request
+router.get('/notes', function (req, res) {
+    saveData
+        .retrieveNotes()
+        .then(notes => res.json(notes))
+        .catch(err => res.status(500).json(err));
+});
+
+// post request
+router.post('/notes', (req, res) => {
+    saveData
+        .addNote(req.body)
+        .then((note) => res.json(note))
+        .catch(err => res.status(500).json(err));
+});
+
+// deletes the note with id=req.params.id
+router.delete('/notes/:id', function (req, res) {
+    saveData
+        .deleteNote(req.params.id)
+        .then(() => res.json({ ok: true }))
+        .catch(err => res.status(500).json(err));
+});
+
+
+module.exports = router;
